b87008f6aa8bb55b332157208d0770979e5ff6ac
li0128 <1094246274@qq.com>
2021-11-18 20:05:08 +0800

    Update glob_gray2rgb.py

--- glob_gray2rgb.py
+++ glob_gray2rgb.py
@@ -1,44 +1,10 @@
-# This is a sample Python script.
+from PIL import Image
+import glob, os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import cv2
-import os
-#from PIL import Image
+size = 128, 128
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-def gray2rgb(img):
-    gray = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-    gray_three = cv2.merge([gray, gray, gray])
-    return gray_three
-    #cv2.imshow("img", gray_three)
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-    # ./ is source dir
-    for root, dirs, files in os.walk("./", topdown=False):
-        for name in files:
-            #print(os.path.splitext(name)[0])
-            if (os.path.splitext(name)[-1]==".jpg"):
-                path_name=os.path.join(root, name)
-                rgb=gray2rgb(path_name)
-                # ./result images
-                cv2.imwrite("./result/"+name,rgb)
-                print(path_name)
-        for name in dirs:
-            if (os.path.splitext(name)[-1]==".jpg"):
-                path_name = os.path.join(root, name)
-                rgb=gray2rgb(path_name)
-                cv2.imwrite("./result/"+name,rgb)
-                print(path_name)
-
-    #gray2rgb("1.jpg")
-
-    cv2.waitKey()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for infile in glob.glob("*.jpg"):
+    file, ext = os.path.splitext(infile)
+    with Image.open(infile) as im:
+        im.thumbnail(size)
+        im.save(file + ".thumbnail", "JPEG")
